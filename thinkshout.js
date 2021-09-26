254adb221158f20a9aaa89631d31215940e87690
Christian Burk <omahane@gmail.com>
2021-09-26 14:40:32 -0500

    Cleaned up JS of all the cruft and added a little more structure

--- thinkshout.js
+++ thinkshout.js
@@ -1,60 +1,52 @@
-var messageArea = document.querySelector("#form-wrapper p");
-var hamburger = document.querySelector(".hamburger");
-window.onscroll = function() {changeHeader()};
+window.addEventListener('load', function() {
+    var messageArea = document.querySelector("#form-wrapper p");
+    var hamburger = document.querySelector(".hamburger");
+    reactToScroll();
+    handleSubscription();
+});
 
-      // TODO: use this to try it: https://gomakethings.com/how-to-add-transition-animations-to-vanilla-javascript-show-and-hide-methods/
-      // to make the hamburger button work better
-  hamburger.addEventListener("click", function() {
-    hamburger.classList.toggle("is-active");
-    
-    document.querySelector("#top-nav ul.desktop").classList.toggle("expanded");
-  });
+function reactToScroll() {
+    window.onscroll = function() {changeHeader()};
+    hamburger.addEventListener("click", function() {
+        hamburger.classList.toggle("is-active");
+        document.querySelector("#top-nav ul.desktop").classList.toggle("expanded");
+    });
+}
 
-  // Chris Ferdinandi solution follows
-  // Get the natural height of the element
-  var getHeight = function () {
-      elem.style.display = 'block'; // Make it visible TODO: try to make it flex
-      var height = elem.scrollHeight + 'px'; // Get its height
-      elem.style.display = ''; // Hide it again;
-  }
-
-  function changeHeader() {
+function changeHeader() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.querySelector("#logo-ts").classList.add("small-logo");
       document.querySelector("ul.desktop").classList.add("fly-on-the-wall");
     } else {
         document.querySelector("ul.desktop").classList.remove("fly-on-the-wall");
         document.querySelector("#logo-ts").classList.remove("small-logo");
-
     }
   }
 
-if (!localStorage.getItem("subscribed") == true){
-    var subscribe = document.querySelector("#subscribe");
-   
-    subscribe.addEventListener("click", function(event){
-        event.preventDefault();
-        var emailAddr = document.querySelector("#email").value;
-        if (validateEmail(emailAddr)) {
-            localStorage.setItem("subscribed",true);
-            
-            messageArea.innerHTML = "Thanks for signing up.";
-            document.querySelector("#form-wrapper form").classList.add("hidden");
-        } else {
-            messageArea.innerHTML = "Please enter a valid address.";
-        } 
-    });
-} else {
-    var messageArea = document.querySelector("#form-wrapper p");
-    messageArea.innerHTML = "Thanks for signing up."
-    document.querySelector("#form-wrapper form").classList.add("hidden");
+function handleSubscription() {
+    if (!localStorage.getItem("subscribed") == true){
+        var subscribe = document.querySelector("#subscribe");  
+        subscribe.addEventListener("click", function(event){
+            event.preventDefault();
+            var emailAddr = document.querySelector("#email").value;
+            if (validateEmail(emailAddr)) {
+                localStorage.setItem("subscribed",true);     
+                messageArea.innerHTML = "Thanks for signing up.";
+                document.querySelector("#form-wrapper form").classList.add("hidden");
+            } else {
+                messageArea.innerHTML = "Please enter a valid address.";
+            } 
+        });
+    } else {
+        var messageArea = document.querySelector("#form-wrapper p");
+        messageArea.innerHTML = "Thanks for signing up."
+        document.querySelector("#form-wrapper form").classList.add("hidden");
+    }
 }
-  
+
 function validateEmail(input) {
-    console.log("Here's the input " + input);
     var valid = false;
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    console.log(input);
     if (input.match(validRegex)) {
         valid = true;
     } 
